36dfb7a1c5d4b36b4824259dca5e8f7408146ea8
Pete St. Martin <peter.r.stmartin@gmail.com>
2020-12-01 22:54:28 -0600

    Unit tests added to reducers

---
+++ src/redux/reducers/FCReducer.test.js
@@ -0,0 +1,9 @@
+import FCReducer from "./FCReducer";
+
+// FCReducer should initially be an empty array
+test("FCReducer should initially be an empty array", () => {
+  const action = {};
+  const returnedState = FCReducer(undefined, action);
+  expect(Array.isArray(returnedState)).toBe(true);
+  expect(returnedState.length).toBe(0);
+});
